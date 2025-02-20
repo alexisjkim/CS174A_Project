edb2878896d367bc1bf4b0cd4b38810d218d53d6
Daniel Chen <daniel@chenhome.org>
2025-02-20 15:39:47 -0800

    function for projection from 4d to 3d

--- main.js
+++ main.js
@@ -77,6 +77,34 @@ function createAxisLine(color, start, end) {
     return new THREE.Line(geometry, material);
 }
 
+ /** Given a 4d vector, return its perspective or orthographic projection onto 3d.
+  * vector4D: Initial vector
+  * cameraPosition: position of 3d axis in 4d space
+  * cameraMatrix: matrix that transforms to camera basis
+  * d: depth factor (only perspective projection)
+  * perspective: use perspective or orthographic projection
+  */
+function project4DTo3D (vector4D, cameraPosition4D, cameraBasis4D, d, perspective) {
+    // transform 4d vector by subtracting camera position and multiplying by the inverse of the cameraBasis
+    let transformedVector = new THREE.Vector4().subVectors(vector4D, cameraPosition4D);
+    let inverseCameraBasis = new THREE.Matrix4().copy(cameraBasis4D).invert();
+    transformedVector.applyMatrix4(inverseCameraBasis);
+
+    // project to 3d, with perspective or orthographic projection
+    let { x, y, z, w } = transformedVector;
+    if (perspective) {
+        // perspective projection: x', y', z' = x/w, y/w, z/w
+        let scaleFactor = w/d;
+        if (scaleFactor === 0) scaleFactor = 1e-6; // no div by zero
+        projectedVector = new THREE.Vector3(x / scaleFactor, y / scaleFactor, z / scaleFactor);
+    } else {
+        // orthographic projection = drop the w-coordinate
+        projectedVector = new THREE.Vector3(x, y, z);
+    }
+
+    return projectedVector;
+}
+
 // Create axis lines
 const xAxis = createAxisLine(0xff0000, new THREE.Vector3(0, 0, 0), new THREE.Vector3(5, 0, 0)); // Red
 const yAxis = createAxisLine(0x00ff00, new THREE.Vector3(0, 0, 0), new THREE.Vector3(0, 5, 0)); // Green
