316a913764115670a891df6335ade8466d60dac6
t1ffanyc <tchen1515@gmail.com>
2025-02-21 01:11:37 -0800

    no hard coding >:(

--- main.js
+++ main.js
@@ -151,40 +151,16 @@ for(let i = 0; i < 16; i++) {
 
 /* Edges connecting the vertices; should stay the same regardless of vertex dimension */
 
-const edges = [
-    [0, 1],
-    [0, 2],
-    [0, 4],
-    [0, 8],
-    [1, 3],
-    [1, 5],
-    [1, 9],
-    [2, 3],
-    [2, 6],
-    [2, 10],
-    [3, 7],
-    [3, 11],
-    [4, 5],
-    [4, 6],
-    [4, 12],
-    [5, 7],
-    [5, 13],
-    [6, 7],
-    [6, 14],
-    [7, 15],
-    [8, 9],
-    [8, 10],
-    [8, 12],
-    [9, 11],
-    [9, 13],
-    [10, 11],
-    [10, 14],
-    [11, 15],
-    [12, 13],
-    [12, 14],
-    [13, 15],
-    [14, 15]
-];
+const edges = [];
+
+for(let i = 0; i < 15; i++) {
+    for(let j = i+1; j < 16; j++) {
+        let diff = i ^ j;  // XOR to find differing bits
+        if ((diff & (diff - 1)) === 0) { // Check if only one bit is different
+            edges.push([i, j]);
+        }
+    }
+}
 
 console.log("vertices before projection: ", vertices4d);
 
